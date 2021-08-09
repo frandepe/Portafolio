9b6f3bf9a686e6fe4389a69eb3d7ba125fc62669
Franco De Paulo <frandepaulo@yahoo.com.ar>
2021-08-09 17:48:53 -0300

    Portfolio

---
+++ scroll.js
@@ -0,0 +1,135 @@
+window.sr = ScrollReveal();
+
+    // sr.reveal('.container-nav', {
+    //     duration: 2500,
+    //     origin: 'bottom',
+    //     distance: '-100px'
+    // });
+
+    // sr.reveal('.foto-mia', {
+    //     duration: 1000,
+        
+    // });
+    
+    // sr.reveal('.hola-nombre', {
+    //     duration: 1500,
+    //     origin: 'right',
+    //     distance: '-150px'
+    // });
+
+    // sr.reveal('.Franco', {
+    //     duration: 2300,
+    //     origin: 'left',
+    //     distance: '-150px'
+    // });
+
+    // sr.reveal('.De', {
+    //     duration: 2600,
+    //     origin: 'left',
+    //     distance: '-150px'
+    // });
+
+    // sr.reveal('.Paulo', {
+    //     duration: 2900,
+    //     origin: 'left',
+    //     distance: '-150px'
+    // });
+
+    // sr.reveal('.presentacion-descripcion', {
+    //     duration: 4000,
+        
+    // });
+
+    sr.reveal('.logoF', {
+        duration: 500,
+        rotate: {
+            y: 0,
+            x: 500,
+
+        }
+    });
+
+    sr.reveal('.cafecito', {
+        duration: 5500,
+        origin: 'bottom',
+        distance: '-100px'
+        
+        
+    });
+
+    /* ***** PRESENTACION ***** */
+
+    sr.reveal('.foto', {
+        duration: 1000,
+        origin: 'right',
+        distance: '-250px'
+        
+    });
+
+    sr.reveal('.presentacion', {
+        duration: 2000,
+        origin: 'left',
+        distance: '-250px'
+    });
+
+    sr.reveal('.sobre-mi', {
+        duration: 3000,
+        origin: 'top',
+        distance: '-200px'
+    });
+
+    sr.reveal('.educacion', {
+        duration: 4000,
+        origin: 'top',
+        distance: '-200px'
+    });
+
+    /* ***** HABILIDADES ***** */
+    
+    sr.reveal('.icono-star', {
+        duration: 2000,
+    });
+    
+    sr.reveal('.titulo-skills', {
+        duration: 2000,
+        
+    });
+    
+    sr.reveal('.icono1', {
+        duration: 2000,
+        origin: 'right',
+        distance: '-100px'
+    });
+
+    sr.reveal('.icono2', {
+        duration: 2000,
+        origin: 'left',
+        distance: '-100px'
+    });
+    
+    sr.reveal('.contenedor-skills p', {
+        duration: 3000,
+        
+    });
+
+    /* ***** PROYECTOS ***** */
+
+    sr.reveal('.trabajo1', {
+        duration: 2000,
+    });
+
+    sr.reveal('.trabajo2', {
+        duration: 4000,
+    });
+
+    sr.reveal('.trabajo3', {
+        duration: 6000,
+    });
+
+    /* ***** REDES SOCIALES ***** */
+
+    sr.reveal('.contenedor-redes', {
+        duration: 2000,
+        origin: 'left',
+        distance: '-300px'
+    });
