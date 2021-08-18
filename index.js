6e8a7b02cb7e3d4f81225b99db62f1d2038fc711
Franco De Paulo <frandepaulo@yahoo.com.ar>
2021-08-18 15:21:15 -0300

    commit 4

---
+++ index.js
@@ -0,0 +1,35 @@
+const goTop = document.getElementById("goTop");
+
+
+
+goTop.addEventListener("click", subir);
+
+function subir() {
+    document.body.scrollTop=0;
+}
+
+window.addEventListener("scroll", scrollear);
+
+function scrollear() {
+    if (document.body.scrollTop > 800) {
+        goTop.style.display="flex"
+    } else {
+        goTop.style.display="none"
+    }
+    
+}
+
+window.addEventListener("scroll", cambiarColorScroll);
+
+function cambiarColorScroll() {
+    if (document.body.scrollTop > 1500) {
+        goTop.style.color="#191A1E"
+    } else {
+        goTop.style.color="#F2ED54";
+    }
+
+    if (document.body.scrollTop > 2800){
+        goTop.style.color="#F2ED54";
+    }
+    
+}
