54b3bd55bb346f7a15ac173b536d6ee90533f0a8
Franco De Paulo <frandepaulo@yahoo.com.ar>
2021-12-15 20:19:01 -0300

    english

--- contacto.js
+++ contacto.js
@@ -1,30 +1,27 @@
-const urlBase = 'https://formspree.io/f/xjvjjwjp';
+const urlBase = "https://formspree.io/f/xjvjjwjp";
 
-const $form = document.querySelector('#form');
-const mensajeDeEnvio = document.getElementById('mensajeDeEnvio')
+const $form = document.querySelector("#form");
+const mensajeDeEnvio = document.getElementById("mensajeDeEnvio");
 
-$form.addEventListener('submit', enviarForm);
+$form.addEventListener("submit", enviarForm);
 
-async function enviarForm (e) {
-    try{
-        e.preventDefault();
-        const formulario = new FormData($form)
-        const response = await fetch(urlBase, {
-            method: 'POST',
-            body: formulario,
-            headers: {
-                'Accept': 'application/json'
-            }
-        });
-        if (response.ok) {
-        this.reset()
-        mensajeDeEnvio.innerText = 'Gracias por contactarme, responderé a la brevedad :)'
-        }
-    }catch(error){
-        console.log('error', error);
+async function enviarForm(e) {
+  try {
+    e.preventDefault();
+    const formulario = new FormData($form);
+    const response = await fetch(urlBase, {
+      method: "POST",
+      body: formulario,
+      headers: {
+        Accept: "application/json",
+      },
+    });
+    if (response.ok) {
+      this.reset();
+      mensajeDeEnvio.innerText =
+        "Thank you for contacting me, I will respond as soon as possible :)";
     }
-    
-    
+  } catch (error) {
+    console.log("error", error);
+  }
 }
-
-
