526c74cdb5eaea9bf2b7a0a6fd8aeb8f3739a855
Franco De Paulo <frandepaulo@yahoo.com.ar>
2022-05-21 01:48:14 -0300

    new port

--- index.js
+++ index.js
@@ -16,26 +16,31 @@ function scrollear() {
   }
 }
 
-// window.addEventListener("scroll", cambiarColorScroll);
-
-// function cambiarColorScroll() {
-//     if (document.body.scrollTop > 1500) {
-//         goTop.style.color="#191A1E"
-//     } else {
-//         goTop.style.color="#F2ED54";
-//     }
-
-//     if (document.body.scrollTop > 2800){
-//         goTop.style.color="#F2ED54";
-//     }
-
-// }
-
-//HAM
+// HAM
 
 const ham = document.querySelector(".ham");
 const li = document.querySelector(".lista-navegacion");
 
 ham.addEventListener("click", () => {
   li.classList.toggle("activado");
+});
+
+// CONTACT
+
+let messageBox = document.querySelector(".js-message");
+let btn = document.querySelector(".js-message-btn");
+let card = document.querySelector(".js-profile-card");
+let closeBtn = document.querySelectorAll(".js-message-close");
+
+btn.addEventListener("click", function (e) {
+  e.preventDefault();
+  card.classList.add("active");
+});
+
+closeBtn.forEach(function (element, index) {
+  console.log(element);
+  element.addEventListener("click", function (e) {
+    e.preventDefault();
+    card.classList.remove("active");
+  });
 });
